c278d69bcc78e1f0374816bdac43e975b972d187
eerrly <chenerlishuaige@163.com>
2023-09-19 19:29:18 +0800

    NetConstant

---
+++ NetConstant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERIUnitySimpleServer
+{
+    class NetConstant
+    {
+        public static readonly string IP = "127.0.0.1";
+
+        public static readonly int Port = 10086;
+
+        public static readonly int FrameInterval = 100;
+
+        public static readonly int RecvTimeOut = 10000;
+
+        public static readonly int RecvTimeOutFrame = RecvTimeOut / FrameInterval;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,28 +5,23 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.IO;
+using System.Linq;
 
 namespace ERIUnitySimpleServer
 {
-    static class Constant
-    {
-        public static readonly string IP = "127.0.0.1";
-
-        public static readonly int Port = 10086;
-
-        public static readonly int FrameInterval = 100;
-
-        public static readonly int RecvTimeOut = 10000;
-
-        public static readonly int RecvTimeOutFrame = RecvTimeOut / FrameInterval;
-    }
-
+    /// <summary>
+    /// 行为
+    /// </summary>
     enum ACT
     {
         HEARTBEAT,
         DATA,
+        JOIN,
     }
 
+    /// <summary>
+    /// 头数据
+    /// </summary>
     public struct Head
     {
         public int size;
@@ -37,6 +32,9 @@ namespace ERIUnitySimpleServer
         public static readonly int EndPointLength = 16;
     }
 
+    /// <summary>
+    /// 包体
+    /// </summary>
     public struct Packet
     {
         public Head head;
@@ -45,17 +43,25 @@ namespace ERIUnitySimpleServer
 
     class Program
     {
+        private static readonly int MaxClientCount = 2;
+
         private static Socket socket;
         private static Queue<Packet> sendQueue = new Queue<Packet>();
         private static Queue<Packet> recvQueue = new Queue<Packet>();
         private static byte[] sendBuffer;
         private static byte[] recvBuffer = new byte[1024];
         private static EndPoint endPoint;
-        private static Dictionary<EndPoint, byte[]> clientPointList = new Dictionary<EndPoint, byte[]>();
-        private static Dictionary<EndPoint, int> frameList = new Dictionary<EndPoint, int>();
+        private static Dictionary<EndPoint, byte[]> clientPointDic = new Dictionary<EndPoint, byte[]>(MaxClientCount);
+        private static Dictionary<EndPoint, int> frameDic = new Dictionary<EndPoint, int>(MaxClientCount);
+        private static Dictionary<EndPoint, int> joinDic = new Dictionary<EndPoint, int>(MaxClientCount);
         private static List<EndPoint> disconnectEndPoints = new List<EndPoint>();
         private static int currentFrame = 0;
 
+        /// <summary>
+        /// EndPoint转字节数组
+        /// </summary>
+        /// <param name="clientEndPoint"></param>
+        /// <returns></returns>
         private static byte[] EndPointToBytes(EndPoint clientEndPoint)
         {
             SocketAddress socketAddress = clientEndPoint.Serialize();
@@ -67,6 +73,11 @@ namespace ERIUnitySimpleServer
             return buffer;
         }
 
+        /// <summary>
+        /// 字节数组转EndPoint
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
         private static EndPoint BytesToEndPoint(byte[] data)
         {
             EndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
@@ -78,11 +89,14 @@ namespace ERIUnitySimpleServer
             return clientEndPoint.Create(socketAddress);
         }
 
+        /// <summary>
+        /// 初始化UDP
+        /// </summary>
         private static void InitUDP()
         {
-            endPoint = new IPEndPoint(IPAddress.Parse(Constant.IP), Constant.Port);
+            endPoint = new IPEndPoint(IPAddress.Parse(NetConstant.IP), NetConstant.Port);
             socket = new Socket(endPoint.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
-            socket.ReceiveTimeout = Constant.RecvTimeOut;
+            socket.ReceiveTimeout = NetConstant.RecvTimeOut;
 
             uint IOC_IN = 0x80000000;
             uint IOC_VENDOR = 0x18000000;
@@ -98,6 +112,9 @@ namespace ERIUnitySimpleServer
             }
         }
 
+        /// <summary>
+        /// 初始化日志打印
+        /// </summary>
         private static void InitLogger()
         {
             Logger.Initialize(Path.Combine(Directory.GetCurrentDirectory(), "server.log"), new Logger());
@@ -106,6 +123,9 @@ namespace ERIUnitySimpleServer
             Logger.logError = Console.WriteLine;
         }
 
+        /// <summary>
+        /// 发送数据线程
+        /// </summary>
         private static void SendThreadMethod()
         {
             while (socket != null)
@@ -115,7 +135,7 @@ namespace ERIUnitySimpleServer
                     if(sendQueue.Count > 0)
                     {
                         var packetInfo = sendQueue.Dequeue();
-                        foreach (var pointInfo in clientPointList)
+                        foreach (var pointInfo in clientPointDic)
                         {
                             sendBuffer = BufferPool.GetBuffer(Head.Length + Head.EndPointLength + packetInfo.head.size);
                             unsafe
@@ -137,6 +157,9 @@ namespace ERIUnitySimpleServer
             }
         }
 
+        /// <summary>
+        /// 接受数据线程
+        /// </summary>
         private static void RecvThreadMethod()
         {
             while (socket != null)
@@ -149,10 +172,10 @@ namespace ERIUnitySimpleServer
                         lock (recvQueue)
                         {
                             byte[] clientEndPointBytes;
-                            if (!clientPointList.TryGetValue(clientPoint, out clientEndPointBytes))
+                            if (!clientPointDic.TryGetValue(clientPoint, out clientEndPointBytes))
                             {
                                 clientEndPointBytes = EndPointToBytes(clientPoint);
-                                clientPointList.Add(clientPoint, clientEndPointBytes);
+                                clientPointDic.Add(clientPoint, clientEndPointBytes);
                             }
                             Packet packet = new Packet();
                             unsafe
@@ -178,6 +201,9 @@ namespace ERIUnitySimpleServer
             }
         }
 
+        /// <summary>
+        /// 固定帧轮询
+        /// </summary>
         private static void Update(object state)
         {
             currentFrame += 1;
@@ -192,27 +218,45 @@ namespace ERIUnitySimpleServer
                         case ACT.HEARTBEAT:
                             {
                                 Logger.Log(LogLevel.Info, $"【客户端】帧:{currentFrame} IP地址:{clientEndPoint} 类型:{Enum.GetName(typeof(ACT), packet.head.act)}");
-                                frameList[clientEndPoint] = currentFrame;
+                                frameDic[clientEndPoint] = currentFrame;
                                 BufferPool.ReleaseBuff(packet.data);
                             }
                             break;
                         case ACT.DATA:
                             {
-                                var message = new byte[(int)packet.head.size];
-                                Array.Copy(packet.data, Head.EndPointLength, message, 0, packet.head.size);
-                                Logger.Log(LogLevel.Info, $"【客户端】帧:{currentFrame} IP地址:{clientEndPoint} 类型:{Enum.GetName(typeof(ACT), packet.head.act)} 序号:{packet.head.index} 数据:{Encoding.UTF8.GetString(message)}");
+                                var realData = BufferPool.GetBuffer(packet.head.size);
+                                Array.Copy(packet.data, Head.EndPointLength, realData, 0, packet.head.size);
+                                Logger.Log(LogLevel.Info, $"【客户端】帧:{currentFrame} IP地址:{clientEndPoint} 类型:{Enum.GetName(typeof(ACT), packet.head.act)} 序号:{packet.head.index} 数据:{Encoding.UTF8.GetString(realData)}");
+                                BufferPool.ReleaseBuff(realData);
                                 lock (sendQueue)
                                 {
                                     sendQueue.Enqueue(packet);
                                 }
                             }
                             break;
+                        case ACT.JOIN:
+                            {
+                                var realData = BufferPool.GetBuffer(packet.head.size);
+                                Array.Copy(packet.data, Head.EndPointLength, realData, 0, packet.head.size);
+                                var joinState = BitConverter.ToInt32(realData, 0);
+                                Logger.Log(LogLevel.Info, $"【客户端】帧:{currentFrame} IP地址:{clientEndPoint} 类型:{Enum.GetName(typeof(ACT), packet.head.act)} 准备状态:{joinState}");
+                                joinDic[clientEndPoint] = joinState;
+                                
+                                if(joinDic.Count >= MaxClientCount && !joinDic.Any((v) => { return v.Value == 0; }))
+                                {
+                                    lock (sendQueue)
+                                    {
+                                        sendQueue.Enqueue(packet);
+                                    }
+                                }
+                            }
+                            break;
                     }
                 }
             }
-            foreach (var item in frameList)
+            foreach (var item in frameDic)
             {
-                if (currentFrame - item.Value >= Constant.RecvTimeOutFrame)
+                if (currentFrame - item.Value >= NetConstant.RecvTimeOutFrame)
                 {
                     disconnectEndPoints.Add(item.Key);
                 }
@@ -220,8 +264,9 @@ namespace ERIUnitySimpleServer
             foreach (var item in disconnectEndPoints)
             {
                 Logger.Log(LogLevel.Info, $"【客户端】 IP地址: {item} 断开连接！");
-                clientPointList.Remove(item);
-                frameList.Remove(item);
+                clientPointDic.Remove(item);
+                frameDic.Remove(item);
+                joinDic.Remove(item);
             }
             if (disconnectEndPoints.Count > 0)
             {
@@ -238,7 +283,7 @@ namespace ERIUnitySimpleServer
 
             Thread sendThread = new Thread(SendThreadMethod);
             Thread recvThread = new Thread(RecvThreadMethod);
-            Timer timer = new Timer(Update, null, 0, Constant.FrameInterval);
+            Timer timer = new Timer(Update, null, 0, NetConstant.FrameInterval);
 
             sendThread.Start();
             recvThread.Start();
