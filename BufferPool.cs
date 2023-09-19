8f9b756ffb9d42cc695f8b54d76aa61f178a17df
eerrly <chenerlishuaige@163.com>
2023-09-19 13:49:04 +0800

    namespace

--- BufferPool.cs
+++ BufferPool.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Test
+namespace ERIUnitySimpleServer
 {
     class BufferPool
     {
--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-namespace Test
+namespace ERIUnitySimpleServer
 {
     static class Constant
     {
