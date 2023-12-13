2c4da1573f9dba4f68fe363bf085ef0dabba88c3
eerrly <chenerlishuaige@163.com>
2023-12-13 19:39:37 +0800

    KCP服务器初始化 Step0

--- NetConstant.cs
+++ NetConstant.cs
@@ -8,6 +8,9 @@
 
         public static readonly int FrameInterval = 33;
 
+    	/// <summary>
+    	/// 帧数据命令
+    	/// </summary>
         public const int pvpFrameCmd = 4;
         public const int pvpReadyAct = 3;
         public const int pvpFrameAct = 2;
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
@@ -10,34 +11,58 @@ namespace ERIUnitySimpleServer
 {
     public class CacheUnit
     {
-        public byte[] data;
-        public int length;
+        public byte[] Data;
+        public int Length;
+    }
+
+    public class ClientInfo
+    {
+        public byte Pos;
+        public Dictionary<int, byte> frameInputDic;
+        public bool IsReady;
+        public EndPoint ClientEndPoint;
+
+        public ClientInfo(byte pos)
+        {
+            Pos = pos;
+            IsReady = false;
+            ClientEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            frameInputDic = new Dictionary<int, byte>();
+        }
     }
     
     class Program
     {
         private static KCP _kcp;
-        private static Socket socket;
-        private static EndPoint endPoint;
-        
+        private static Socket _socket;
+        private static EndPoint _endPoint;
+        private static EndPoint _clientPoint;
+
         private static ushort _index = 0;
         private static byte[] _sendBuffer = new byte[Head.MaxSendLength];
         private static byte[] _recvBuffer = new byte[Head.MaxRecvLength];
         private static EndPoint clientPoint;
-        
+
         private static Queue<Packet> _sendQueue = new Queue<Packet>();
-        private static Queue<Packet> recvQueue = new Queue<Packet>();
+        private static Queue<Packet> _recvQueue = new Queue<Packet>();
+        private static MemoryStream _recvStream;
+        private static BinaryReader _binaryReader;
 
         private static object _sendLock = new object();
         private static Queue<CacheUnit> _finalSendQueue = new Queue<CacheUnit>();
-        
+
         private static readonly DateTime m_UtcTime = new DateTime(1970, 1, 1);
+
+        private static int _lastRecvFrame = 0;
+        private static Dictionary<byte, ClientInfo> _clientInfoDic = new Dictionary<byte, ClientInfo>();
+
         private static UInt32 iclock()
         {
             return (UInt32)(Convert.ToInt64(DateTime.UtcNow.Subtract(m_UtcTime).TotalMilliseconds) & 0xffffffff);
         }
-        
+
         private static Stopwatch _watch;
+
         public static long Time
         {
             get
@@ -47,10 +72,11 @@ namespace ERIUnitySimpleServer
                     _watch = new Stopwatch();
                     _watch.Start();
                 }
+
                 return _watch.ElapsedMilliseconds;
             }
         }
-        
+
         /// <summary>
         /// 初始化日志打印
         /// </summary>
@@ -61,38 +87,43 @@ namespace ERIUnitySimpleServer
             Logger.log = Console.WriteLine;
             Logger.logError = Console.WriteLine;
         }
-        
+
         /// <summary>
         /// 初始化UDP服务器
         /// </summary>
         private static void InitUDP()
         {
-            endPoint = new IPEndPoint(IPAddress.Parse(NetConstant.IP), NetConstant.Port);
-            socket = new Socket(endPoint.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
-            socket.ReceiveTimeout = NetConstant.FrameInterval * 10;
+            _endPoint = new IPEndPoint(IPAddress.Parse(NetConstant.IP), NetConstant.Port);
+            _socket = new Socket(_endPoint.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
+            _socket.ReceiveTimeout = NetConstant.FrameInterval * 10;
 
             uint IOC_IN = 0x80000000;
             uint IOC_VENDOR = 0x18000000;
             uint SIO_UDP_CONNRESET = IOC_IN | IOC_VENDOR | 12;
-            socket.IOControl((int)SIO_UDP_CONNRESET, new byte[] { Convert.ToByte(false) }, null);
+            _socket.IOControl((int)SIO_UDP_CONNRESET, new byte[] { Convert.ToByte(false) }, null);
             try
             {
-                socket.Bind(endPoint);
+                _socket.Bind(_endPoint);
             }
             catch (Exception ex)
             {
-                Logger.Log(LogLevel.Exception, $"[初始化UDP服务器 异常] 错误信息: {ex.Message}");
+                Logger.Log(LogLevel.Exception, $"[初始化UDP服务器] 错误信息: {ex.Message}");
             }
+
             try
             {
                 Kcp_Init(0);
             }
             catch (Exception ex)
             {
-                Logger.Log(LogLevel.Exception, $"[初始化KCP服务器 异常] 错误信息: {ex.Message}");
+                Logger.Log(LogLevel.Exception, $"[初始化KCP服务器] 错误信息: {ex.Message}");
             }
         }
-        
+
+        /// <summary>
+        /// 初始化KCP
+        /// </summary>
+        /// <param name="conv">会话ID</param>
         private static void Kcp_Init(uint conv)
         {
             if (_kcp != null)
@@ -103,18 +134,24 @@ namespace ERIUnitySimpleServer
             {
                 _kcp = new KCP(conv, Kcp_Send);
             }
+
             _kcp.NoDelay(1, 1, 2, 1); //快速模式
             _kcp.WndSize(512, 512);
         }
-        
+
+        /// <summary>
+        /// KCP发送消息回调
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="size"></param>
         private static void Kcp_Send(byte[] data, int size)
         {
             var unit = new CacheUnit
             {
-                data = BufferPool.GetBuffer(size),
-                length = size
+                Data = BufferPool.GetBuffer(size),
+                Length = size
             };
-            Array.Copy(data, unit.data, size);
+            Array.Copy(data, unit.Data, size);
 
             lock (_sendLock)
             {
@@ -124,24 +161,99 @@ namespace ERIUnitySimpleServer
 
         static void Main(string[] args)
         {
+            _recvStream = new MemoryStream(256);
+            _binaryReader = new BinaryReader(_recvStream);
+
             InitLogger();
             InitUDP();
-            
+
             var thread = new Thread(UpdateThreadMethod);
+            var timer = new Timer(Update, null, 0, NetConstant.FrameInterval);
             thread.Start();
         }
 
         private static int _usedRecvBufferSize = 0;
+        private static int _serverFrame = -1;
+        private static bool _isAllReady = false;
+
+        private static void Update(object state)
+        {
+            if (!_isAllReady) return;
+
+            if (_serverFrame >= 0)
+            {
+                using (var stream = new MemoryStream())
+                {
+                    using (var writer = new BinaryWriter(stream))
+                    {
+                        writer.Write(_serverFrame);
+                        writer.Write((byte)2);
+                        var i0 = (byte)0;
+                        var i1 = (byte)1;
+                        if (_clientInfoDic.ContainsKey(0) && _clientInfoDic[0].frameInputDic.ContainsKey(_serverFrame)) 
+                            i0 = _clientInfoDic[0].frameInputDic[_serverFrame];
+                        else if (_clientInfoDic.ContainsKey(0) && _clientInfoDic[0].frameInputDic.ContainsKey(_lastRecvFrame)) 
+                            i0 = _clientInfoDic[0].frameInputDic[_lastRecvFrame];
+                        if (_clientInfoDic.ContainsKey(1) && _clientInfoDic[1].frameInputDic.ContainsKey(_serverFrame)) 
+                            i1 = _clientInfoDic[1].frameInputDic[_serverFrame];
+                        else if (_clientInfoDic.ContainsKey(1) && _clientInfoDic[1].frameInputDic.ContainsKey(_lastRecvFrame)) 
+                            i1 = _clientInfoDic[1].frameInputDic[_lastRecvFrame];
+                        
+                        writer.Write(i0);
+                        writer.Write(i1);
+
+                        var len = (int)stream.Position;
+                        var buffer = BufferPool.GetBuffer(len);
+                        stream.Seek(0, SeekOrigin.Begin);
+                        stream.Read(buffer, 0, len);
+
+                        Logger.Log(LogLevel.Info, $"给客户端发送帧数据 [serverFrame]->{_serverFrame} [i0]->{i0} [i1]->{i1}");
+                        SendDataToClient(buffer, NetConstant.pvpFrameCmd, NetConstant.pvpFrameAct, len);
+                    }
+                }
+            }
+            _serverFrame++;
+            // lock (_recvQueue)
+            // {
+            //     while (_recvQueue.Count > 0)
+            //     {
+            //         var packet = _recvQueue.Dequeue();
+            //
+            //         var _stream = new MemoryStream(packet.Data);
+            //         var _reader = new BinaryReader(_stream);
+            //         var frameId = _reader.ReadInt32();
+            //         var input = _reader.ReadByte();
+            //         
+            //         using (var stream = new MemoryStream())
+            //         {
+            //             using (var writer = new BinaryWriter(stream))
+            //             {
+            //                 writer.Write(_serverFrame);
+            //                 writer.Write((byte)1);
+            //                 writer.Write(input);
+            //                 
+            //                 var len = (int)stream.Position;
+            //                 var buffer = BufferPool.GetBuffer(len);
+            //                 stream.Seek(0, SeekOrigin.Begin);
+            //                 stream.Read(buffer, 0, len);
+            //             
+            //                 SendDataToClient(buffer, packet.Head.cmd, packet.Head.act, len);
+            //             }
+            //         }
+            //     }
+            // }
+        }
+
         /// <summary>
-        /// 接受数据线程
+        /// 轮询
         /// </summary>
         private static void UpdateThreadMethod()
         {
-            while (socket != null && _kcp != null)
+            while (_socket != null && _kcp != null)
             {
                 lock (_sendLock)
                 {
-                    while (_sendQueue.Count > 0 && _kcp != null && _kcp.WaitSnd() < 512)
+                    while (_sendQueue.Count > 0 && _kcp.WaitSnd() < 512)
                     {
                         var packet = _sendQueue.Dequeue();
                         unsafe
@@ -153,14 +265,15 @@ namespace ERIUnitySimpleServer
                                 packet.Head.index = _index++;
                                 *((Head*)dest) = packet.Head;
                             }
+
                             Array.Copy(packet.Data, 0, sendBuffer, Head.Length, packet.Head.length);
                             BufferPool.ReleaseBuff(packet.Data);
-                        
+
                             _kcp.Send(sendBuffer, 0, length);
                         }
                     }
                 }
-                
+
                 var needFlush = false;
                 while (_finalSendQueue.Count > 0)
                 {
@@ -173,31 +286,34 @@ namespace ERIUnitySimpleServer
                     try
                     {
                         needFlush = true;
-                        socket.SendTo(unit.data, 0, unit.length, SocketFlags.None, clientPoint);
+                        foreach (var info in _clientInfoDic)
+                        {
+                            _socket.SendTo(unit.Data, 0, unit.Length, SocketFlags.None, info.Value.ClientEndPoint);
+                        }
                     }
                     catch (Exception ex)
                     {
-                        Logger.Log(LogLevel.Exception, $"发送数据发生异常 错误:{ex.Message}");
+                        Logger.Log(LogLevel.Exception, $"[发送数据] 错误:{ex.Message}");
                     }
                     finally
                     {
                         if (unit != null)
                         {
-                            BufferPool.ReleaseBuff(unit.data);
+                            BufferPool.ReleaseBuff(unit.Data);
                             unit = null;
                         }
                     }
                 }
 
                 _kcp.Update(iclock(), needFlush);
-                
+
                 var size = Head.MaxRecvLength - _usedRecvBufferSize;
                 if (size > 0)
                 {
-                    clientPoint = new IPEndPoint(IPAddress.Any, 0);
+                    if (_clientPoint == null) _clientPoint = new IPEndPoint(IPAddress.Any, 0);
                     try
                     {
-                        var length = socket.ReceiveFrom(_recvBuffer, 0, _recvBuffer.Length, SocketFlags.None, ref clientPoint);
+                        var length = _socket.Available > 0 ? _socket.ReceiveFrom(_recvBuffer, 0, _recvBuffer.Length, SocketFlags.None, ref _clientPoint) : 0;
                         if (length > 0)
                         {
                             _usedRecvBufferSize += length;
@@ -240,23 +356,105 @@ namespace ERIUnitySimpleServer
                                     packet.Length = (int)packet.Head.length;
                                     Array.Copy(data, Head.Length, packet.Data, 0, (int)packet.Head.length);
 
-                                    packet.RecvTime = Time;
+                                    // Logger.Log(LogLevel.Info, $"收到数据 cmd:{packet.Head.cmd} act:{packet.Head.act} length:{packet.Head.length}");
 
-                                    recvQueue.Enqueue(packet);
+                                    _recvStream.Position = 0;
+                                    _recvStream.SetLength(0);
+                                    _recvStream.Write(packet.Data, 0, (int)packet.Head.length);
+                                    _recvStream.Seek(0, SeekOrigin.Begin);
+
+                                    if (packet.Head.act == NetConstant.pvpReadyAct)
+                                    {
+                                        var frameId = _binaryReader.ReadInt32();
+                                        var playerId = _binaryReader.ReadByte();
+                                        Logger.Log(LogLevel.Info, $"收到客户端发来的准备数据 [frameId]->{frameId} [playerId]->{playerId}");
+                                        
+                                        if(!_clientInfoDic.ContainsKey(playerId)) _clientInfoDic[playerId] = new ClientInfo(playerId);
+                                        _clientInfoDic[playerId].IsReady = true;
+                                        _isAllReady = _clientInfoDic.Count == 2 && _clientInfoDic.All(t => t.Value.IsReady != false);
+
+                                        if (_isAllReady)
+                                        {
+                                            SendDataToClient(packet.Data, packet.Head.cmd, packet.Head.act, (int)packet.Head.length);
+                                        }
+                                    }
+                                    else if (packet.Head.act == NetConstant.pvpPingAct)
+                                    {
+                                        var clientTime = _binaryReader.ReadInt64();
+                                        Logger.Log(LogLevel.Info, $"收到客户端发来的Ping数据 [clientTime]->{clientTime}");
+                                        SendDataToClient(packet.Data, packet.Head.cmd, packet.Head.act, (int)packet.Head.length);
+                                    }
+                                    else if (packet.Head.act == NetConstant.pvpFrameAct)
+                                    {
+                                        var frameId = _binaryReader.ReadInt32();
+                                        var input = _binaryReader.ReadByte();
+                                        var pos = (byte)(0x01 & input);
+                                        Logger.Log(LogLevel.Info, $"收到客户端发来的操作数据 [pos]->{pos} [frameId]->{frameId} [input]->{input}");
+
+                                        _lastRecvFrame = frameId;
+                                        _clientInfoDic[pos].frameInputDic[frameId] = input;
+                                        
+                                        // lock (_recvQueue)
+                                        // {
+                                        //     _recvQueue.Enqueue(packet);
+                                        // }
+                                        // using (var stream = new MemoryStream())
+                                        // {
+                                        //     using (var writer = new BinaryWriter(stream))
+                                        //     {
+                                        //         writer.Write(frameId);
+                                        //         writer.Write((byte)1);
+                                        //         writer.Write(input);
+                                        //         
+                                        //         var len = (int)stream.Position;
+                                        //         var buffer = BufferPool.GetBuffer(len);
+                                        //         stream.Seek(0, SeekOrigin.Begin);
+                                        //         stream.Read(buffer, 0, len);
+                                        //     
+                                        //         SendDataToClient(buffer, packet.Head.cmd, packet.Head.act, len);
+                                        //     }
+                                        // }
+                                    }
                                 }
                             }
                         }
                     }
                     catch (SocketException ex)
                     {
-                        Logger.Log(LogLevel.Exception, $"[接受数据 异常] 错误码: {ex.ErrorCode} 错误信息: {ex.Message}");
+                        Logger.Log(LogLevel.Exception, $"[接受数据] 错误码: {ex.ErrorCode} 错误信息: {ex.Message}");
                     }
                     catch (Exception ex)
                     {
-                        Logger.Log(LogLevel.Exception, $"[接受数据 异常] 错误信息: {ex.Message}");
+                        Logger.Log(LogLevel.Exception, $"[接受数据] 错误信息: {ex.Message}");
                     }
                 }
+
                 Thread.Sleep(1);
+            }
+        }
+
+        /// <summary>
+        /// 发送数据给服务器
+        /// </summary>
+        /// <param name="data">数据</param>
+        /// <param name="cmd">命令</param>
+        /// <param name="act">动作</param>
+        /// <param name="length">数据长度</param>
+        private static void SendDataToClient(byte[] data, int cmd, int act, int length)
+        {
+            lock (_sendLock)
+            {
+                var packet = new Packet();
+                var head = new Head
+                {
+                    cmd = (byte)cmd,
+                    act = (byte)act,
+                    length = (uint)length
+                };
+                packet.Head = head;
+                packet.Data = data;
+
+                _sendQueue.Enqueue(packet);
             }
         }
     }
