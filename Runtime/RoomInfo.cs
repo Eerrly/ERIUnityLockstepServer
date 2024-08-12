e5df16bae192f73c23b2d27043edbe5e7b4f9c4a
eerrly <chenerlishuaige@163.com>
2024-08-12 14:57:39 +0800

    添加注释

--- Runtime/RoomInfo.cs
+++ Runtime/RoomInfo.cs
@@ -4,16 +4,25 @@ public class RoomInfo
 {
     public uint RoomId;
     
+    /// <summary>
+    /// 服务器当前帧
+    /// </summary>
     public int AuthoritativeFrame;
     
     public List<uint> Readies;
     
     public List<uint> Gamers;
     
+    /// <summary>
+    /// 每一帧玩家是否操作了的标记数组
+    /// </summary>
     public byte[] InputCounts;
     
     public Stopwatch BattleStopwatch;
 
+    /// <summary>
+    /// 某一帧玩家的MD5校验值数组
+    /// </summary>
     public Dictionary<int, List<int>> BattleCheckMap;
 
     public RoomInfo()
